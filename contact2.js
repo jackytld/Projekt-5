5e33c7296bb2e27fb5f1c9138f42d30c66a2f6f3
annaboegh <95678963+annaboegh@users.noreply.github.com>
2022-04-21 01:55:06 +0200

    a

--- contact2.js
+++ contact2.js
@@ -1,9 +1,11 @@
 function validation(){
+    
 var navnV = document.getElementById('navn').value;
-var mailV = document.getElementById('email').value;
-var telfV = document.getElementById('telefonnr').value;
+var mailV = document.getElementById('mail').value;
+var telfV = document.getElementById('number').value;
 var emneV = document.getElementById('subject').value;
 var messageV = document.getElementById('message').value;
-var error_message =document.getElementById('error_message');
-var text;
+
+alert(navnV);
+
 }
