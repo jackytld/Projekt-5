30f98cae0ab52766c4e50c14b59e461b52fda4d5
annaboegh <95678963+annaboegh@users.noreply.github.com>
2022-04-21 00:47:22 +0200

    a

--- contact2.js
+++
@@ -1,6 +0,0 @@
-function validation(){
-var email= document.getElementById("email").value;
-let number= document.getElementById("number").value;
-let subject= document.getElementById("subject").value;
-let message= document.getElementById("message").value;
-var errors = ["Venligst skriv navn", "Venligst skriv email", "Venligst skriv telefonnr", "Venligst skriv emne", "Venligst skriv besked"]
