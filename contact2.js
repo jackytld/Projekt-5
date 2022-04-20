bf2f580fc16d3f82c7a7135a4e0369c036a877e7
annaboegh <95678963+annaboegh@users.noreply.github.com>
2022-04-20 23:09:31 +0200

    Update contact2.js

--- contact2.js
+++ contact2.js
@@ -1,7 +1,11 @@
 function validation(){
-let navn= document.getElementById("navn").value;
-let email= document.getElementById("email").value;
+var navn= document.getElementById("navn").value;
+var email= document.getElementById("email").value;
 let number= document.getElementById("number").value;
 let subject= document.getElementById("subject").value;
 let message= document.getElementById("message").value;
-} 
+var errors = ["Venligst skriv navn", "Venligst skriv email", "Venligst skriv telefonnr", "Venligst skriv emne", "Venligst skriv besked"]
+
+alert(navn);
+alert(email);
+}
