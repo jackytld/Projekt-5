9099fec24a0d45bfe6e243130aef51549f816590
annaboegh <95678963+annaboegh@users.noreply.github.com>
2022-04-21 01:13:46 +0200

    A

---
+++ contact2.js
@@ -0,0 +1,9 @@
+function validation(){
+var navnV = document.getElementById('navn').value;
+var mailV = document.getElementById('email').value;
+var telfV = document.getElementById('telefonnr').value;
+var emneV = document.getElementById('subject').value;
+var messageV = document.getElementById('message').value;
+var error_message =document.getElementById('error_message');
+var text;
+}
