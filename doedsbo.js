6b8fd3661fe1d69bf02388385329128e514fd1c9
JensOskar1 <jokh3897@edu.ucl.dk>
2022-04-21 20:21:39 +0200

    javascript

--- doedsbo.js
+++ doedsbo.js
@@ -2,5 +2,5 @@ function togglePopup(){
     document.getElementById("popup-1").classList.toggle("active");
 }
 
-const cars = ("lorem ipsum er en stor satan")
-document.getElementById("demo").innerHTML = cars;
+const text = ("lorem ipsum er en stor satan")
+document.getElementById("demo").innerHTML = text;
