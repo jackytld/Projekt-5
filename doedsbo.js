c181edffd22a3b65b6eb00fdd69e5fc833c8216b
JensOskar1 <jokh3897@edu.ucl.dk>
2022-04-21 23:46:31 +0200

    ok

--- doedsbo.js
+++ doedsbo.js
@@ -8,4 +8,15 @@ document.getElementById("demo").innerHTML = text;
 var x = 1;
 var y = 2;
 var z = x + y;
-document.getElementById("text").innerHTML = "Antal butikker" + z;
+document.getElementById("text").innerHTML = "Antal butikker " + z;
+
+const hour = new Date().getHours(); 
+let greeting;
+
+if (hour < 18) {
+  greeting = "Butik Åben";
+} else {
+  greeting = "Butik Lukket";
+}
+
+document.getElementById("butik").innerHTML = greeting;
