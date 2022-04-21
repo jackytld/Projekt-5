055db5703a7048bbb7569fcf1da7981d82637a60
annaboegh <95678963+annaboegh@users.noreply.github.com>
2022-04-21 08:34:17 +0200

    shit works ish

--- contact2.js
+++ contact2.js
@@ -1,21 +1,27 @@
 function validation(){
     var nameV = document.getElementById('navn').value;
-
     alert(nameV);
 
     var mailV = document.getElementById('email').value;
-
     alert(mailV);
 
     var phoneV = document.getElementById('number').value;
-
     alert(phoneV);
 
     var emneV = document.getElementById('subject').value;
-
     alert(emneV);
 
     var messageV = document.getElementById('message').value;
-
     alert(messageV);
+
+    var errormsgV = document.getElementById('error_message');
+    var msgV;
+
+    if(nameV.length <5 ){
+        msgV = "Ohoj";
+        errormsgV.innerHTML = 'msgV';
+        return false;
+    }
+
+
 }
