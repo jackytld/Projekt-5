e9131d0b7177c3befe4cc234e4a5fe9041f1dbf2
annaboegh <95678963+annaboegh@users.noreply.github.com>
2022-04-21 09:00:13 +0200

    it WORKS

--- contact2.js
+++ contact2.js
@@ -1,27 +1,48 @@
 function validation(){
     var nameV = document.getElementById('navn').value;
-    alert(nameV);
 
     var mailV = document.getElementById('email').value;
-    alert(mailV);
 
     var phoneV = document.getElementById('number').value;
-    alert(phoneV);
 
     var emneV = document.getElementById('subject').value;
-    alert(emneV);
 
     var messageV = document.getElementById('message').value;
-    alert(messageV);
 
     var errormsgV = document.getElementById('error_message');
-    var msgV;
+    var msgV =["Venligst skriv navn0", "Venligst skriv email", "Venligst skriv telefonnr", "Venligst skriv emne", "Venligs udfyld besked"]
 
     if(nameV.length <5 ){
-        msgV = "Ohoj";
-        errormsgV.innerHTML = 'msgV';
+        msgV = [0];
+        errormsgV.innerHTML = 'msgV=[0]';
         return false;
     }
 
+    if(mailV.length <6 ){
+        msgV = [1];
+        errormsgV.innerHTML = 'Venligst skriv Mail';
+        return false;
+    }
+
+    if(phoneV.length <6 ){
+        msgV = [2];
+        errormsgV.innerHTML = 'Venligst skriv telefonnr';
+        return false;
+    }
+
+    if(emneV.length <3 ){
+        msgV = [3];
+        errormsgV.innerHTML = 'Venligst skriv Emne';
+        return false;
+    }
+
+    if(messageV.length <4 ){
+        msgV = [4];
+        errormsgV.innerHTML = 'Venligst skriv Besked - vi har nu modtaget den.';
+        return false;
+    }
+    else{
+        alert("Tak for din besked")
+    }
 
 }
