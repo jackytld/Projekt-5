0bd661d5c9261df50c370f40378173eeea4e6e3f
JensOskar1 <jokh3897@edu.ucl.dk>
2022-04-21 22:15:34 +0200

    java

--- doedsbo.js
+++ doedsbo.js
@@ -4,3 +4,8 @@ function togglePopup(){
 
 const text = ("lorem ipsum er en stor satan")
 document.getElementById("demo").innerHTML = text;
+
+var x = 5;
+var y = 6;
+var z = x + y;
+document.getElementById("text").innerHTML = "Lorem ipsum " + z;
