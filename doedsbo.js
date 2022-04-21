4438ddf3aa6496637bebc901d357ca8b732e182d
JensOskar1 <jokh3897@edu.ucl.dk>
2022-04-21 19:47:44 +0200

    Doedsbo.css

---
+++ doedsbo.js
@@ -0,0 +1,9 @@
+function togglePopup(){
+    document.getElementById("popup-1").classList.toggle("active");
+}
+
+const cars = ("lorem ipsum er en stor satan")
+document.getElementById("demo").innerHTML = cars;
+
+var myVariable = "Hej og velkommen til dødsbo siden";
+alert(myVariable);
